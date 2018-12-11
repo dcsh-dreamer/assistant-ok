9eeb82c144f97382c47463de349be7de32105d0a
Dino Wei <idreamer@gmail.com>
2018-12-11 23:24:14 +0800

    登入、登出

--- assistant/urls.py
+++ assistant/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='journal/')), 
     path('journal/', include('journal.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
