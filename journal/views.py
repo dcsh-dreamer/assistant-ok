c4d923817ae645b71c8784ddd34da584af48d9b9
Dino Wei <idreamer@gmail.com>
2018-12-12 00:25:27 +0800

    @login_required 限定登入才能進行操作

--- journal/views.py
+++ journal/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Journal
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 # 日誌列表
+@method_decorator(login_required, name='dispatch')
 class JournalList(ListView):
     model = Journal
     ordering = ['-id']      # 依 id 欄位反向排序(新的在前面)
 
 # 新增日誌
+@method_decorator(login_required, name='dispatch')
 class JournalCreate(CreateView):
     model = Journal
     fields = ['content']            # 自動產生表單時僅顯示 content 欄位
@@ -16,6 +20,7 @@ class JournalCreate(CreateView):
     template_name = 'form.html'
 
 # 修改日誌
+@method_decorator(login_required, name='dispatch')
 class JournalUpdate(UpdateView):
     model = Journal
     fields = ['content']            # 自動產生表單時僅顯示 content 欄位
@@ -23,6 +28,7 @@ class JournalUpdate(UpdateView):
     template_name = 'form.html'
 
 # 刪除日誌
+@method_decorator(login_required, name='dispatch')
 class JournalDelete(DeleteView):
     model = Journal
     success_url = '/journal/'       # 操作成功後重新導向日誌列表頁面
